f640433e973ba884f999a7599e7b47a816db4baf
jaamanu <kennyegun240@gmail.com>
2023-02-26 16:04:55 +0100

    App models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,8 @@
 class Entity < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :entity_groups
+    has_many :groups, through: :entity_groups
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :entities, foreign_key: 'user_id', class_name: 'Entity'
+    has_many :groups, foreign_key: 'user_id', class_name: 'Group'
+
+    validates :name, presence: true, length: { maximum: 100 }
 end
