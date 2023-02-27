d790ad56386f7e5f3a65fe983847c793045bbe6f
jaamanu <kennyegun240@gmail.com>
2023-02-27 22:47:10 +0100

    Mdifies

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :entities, foreign_key: 'user_id', class_name: 'Entity'
-    has_many :groups, foreign_key: 'user_id', class_name: 'Group'
+  has_many :entities, foreign_key: 'user_id', class_name: 'Entity'
+  has_many :groups, foreign_key: 'user_id', class_name: 'Group'
 
-    validates :name, presence: true, length: { maximum: 100 }
+  validates :name, presence: true, length: { maximum: 100 }
 end
